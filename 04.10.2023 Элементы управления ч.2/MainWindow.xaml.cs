9c4806417b117eb1b1a93d705610ff7c5873eb53
Gigg <danya19981017@gmail.com>
2023-10-06 17:41:04 +0200

    Добавьте файлы проекта.

---
+++ 04.10.2023 Элементы управления ч.2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04._10._2023_Элементы_управления_ч._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Image img;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Label_Drop(object sender, DragEventArgs e){
+            ((Image)sender).Source = img.Source;
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e){
+            Image label = sender as Image;
+            img = label;
+
+            DragDrop.DoDragDrop(label, label.Source, DragDropEffects.Copy);
+        }
+    }
+}
